b2d8d63bf09a202946352d17630b4b1e552aa7ce
muzropov <muzropov.oybek@gmail.com>
2020-01-09 14:43:48 +0500

    added resized video

--- generate_url.go
+++ generate_url.go
@@ -5,7 +5,12 @@ import "fmt"
 const URL  = "https://s3.amazonaws.com"
 const BUCKET  = "fiesta.storage-resized"
 const PROMO_VIDEO_PATH  = "videos/original"
+const RESIZED_VIDEO_PATH  = "videos/720p"
 
-func getPromoVideoUrl(filename string) string {
+func GetPromoVideoUrl(filename string) string {
 	return fmt.Sprintf("%s/%s/%s/%s", URL, BUCKET, PROMO_VIDEO_PATH, filename)
+}
+
+func GetResizedVideoUrl(filename string) string {
+	return fmt.Sprintf("%s/%s/%s/%s", URL, BUCKET, RESIZED_VIDEO_PATH, filename)
 }
